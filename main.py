4c7590666eb025953a3af300241913b76c2b7405
nursaProger <nursakeneshbb06@gmail.com>
2024-11-04 10:58:07 +0300

    commit

---
+++ main.py
@@ -0,0 +1,37 @@
+class Person:
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age
+        self.city = city
+
+    def introduce(self):
+        print(f"Привет, меня зовут {self.name},"
+                f" мне {self.age} лет, я живу в {self.city}")
+
+
+
+    def is_adult(self):
+        if self.age >= 18:
+            return print("True")
+        else:
+            return print("False")
+
+
+
+
+
+person = Person("Carl", 20, "Boston")
+person.introduce()
+
+
+leon = Person("Leon", 18, "Moscow")
+leon.is_adult()
+
+alice = Person("Alice", 16, "London")
+alice.is_adult()
+
+dastan = Person("Dastan", 24, "Bishkek")
+dastan.is_adult()
+
+
+
